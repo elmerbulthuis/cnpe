d935985b25613008e44fbeb012ba3731b3aed5e2
Elmer Bulthuis <elmer@luvdasun.software>
2026-07-10 12:07:05 +0200

    terraform wip

--- infrastructure/terraform/local/main.tf
+++ infrastructure/terraform/local/main.tf
@@ -2,22 +2,10 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-resource "kubernetes_namespace" "example" {
-  metadata {
-    name = "example-namespace"
-  }
-}
-
-output "namespace_name" {
-  value       = kubernetes_namespace.example.metadata[0].name
-  description = "Name of the created namespace"
 }
